f8409e213587f7b3e149c2f49215a22e4b1b36d9
pauldambra <paul@posthog.com>
2025-06-11 13:19:50 +0100

    fix

--- src/populate-versions.ts
+++ src/populate-versions.ts
@@ -1,13 +1,20 @@
 import versionsJson from "./versions.json";
 export function populateVersions(selectedVersion?: string) {
+  let lastGroup = "";
   document.getElementById("versions")!.innerHTML = Object.entries(versionsJson)
     .map(([version, config]) => {
       const { rrwebVersion } = config;
       const isDefault = "default" in config && config.default;
-
-      return `<option value="${version}" ${
-        isDefault ? "selected" : ""
-      }>${version} (rrweb v${rrwebVersion})</option>`;
+      const group = config.group;
+      let prefix = "";
+      let suffix = "";
+      if (group !== lastGroup) {
+        lastGroup = group;
+        prefix = `<optgroup label="${group}">`;
+        suffix = `</optgroup>`;
+      }
+      return `${prefix}<option value="${version}" ${isDefault ? "selected" : ""
+        }>${version} (rrweb v${rrwebVersion})</option>${suffix}`;
     })
     .join("");
 
