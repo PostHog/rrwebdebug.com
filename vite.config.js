c9a8d983d915b836ddf7842daa815f6e9c352391
pauldambra <paul@posthog.com>
2025-06-11 13:01:23 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ const { resolve } = require("path");
 const { defineConfig } = require("vite");
 
 module.exports = defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/rrwebdebug.com/' : '/',
   build: {
     rollupOptions: {
       input: {
